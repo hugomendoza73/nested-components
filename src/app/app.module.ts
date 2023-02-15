60839b36964e6ce15ccb0a53d7323d8938fafe48
hugomendoza73 <hmendoza@xdtconsulting.com>
2023-02-15 17:35:20 -0600

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './components/container/container.component';
+import { HeadersComponent } from './components/headers/headers.component';
+import { RowTemplateComponent } from './components/row-template/row-template.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HeadersComponent,
+    RowTemplateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/row-template/row-template.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-row-template',
+  templateUrl: './row-template.component.html',
+  styleUrls: ['./row-template.component.css']
+})
+export class RowTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
